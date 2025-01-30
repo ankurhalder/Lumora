19d0c56ba7b4078bf3ef52153c4b6a9851c46f78
Ankur Halder <ankur.halder12345@gmail.com>
2025-01-30 14:50:14 +0530

    fix: update StatusBar style and simplify RootNavigator usage in App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import { StyleSheet, View, Text, StatusBar } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 import RootNavigator from "./navigation/RootNavigator";
+
 export default function App() {
   return (
     <Fragment>
-      <StatusBar style="auto" backgroundColor="#6a51ae" />
-      <RootNavigator></RootNavigator>
+      <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
+      <RootNavigator />
     </Fragment>
   );
 }
