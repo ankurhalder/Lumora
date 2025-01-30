ad292c83e6519e3c56e88abcfa8862db9f8361d4
Ankur Halder <ankur.halder12345@gmail.com>
2025-01-30 18:03:05 +0530

    refactor: streamline data processing by removing unnecessary comments mapping

--- functions/processData.js
+++ functions/processData.js
@@ -1,11 +1,9 @@
 const processData = (users, posts, comments) => {
-  // Map userId to user object
   const userMap = {};
   users.forEach((user) => {
     userMap[user.id] = user;
   });
 
-  // Map postId to comments array
   const postCommentMap = {};
   comments.forEach((comment) => {
     if (!postCommentMap[comment.postId]) {
@@ -14,7 +12,6 @@ const processData = (users, posts, comments) => {
     postCommentMap[comment.postId].push(comment);
   });
 
-  // Assign users and comments to posts
   const processedPosts = posts.map((post) => ({
     ...post,
     user: userMap[post.userId] || { username: "Unknown", image: "" },
