1b85a676d00aa00bc9620e2b175b803868317c77
Ankur Halder <ankur.halder12345@gmail.com>
2025-01-30 16:10:28 +0530

    feat: implement fetching posts, users, and comments with axios in HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import { fetchPosts } from "../functions/fetchPosts";
 
-function HomeScene() {
+const HomeScreen = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      try {
+        const data = await fetchPosts(0, 10, setLoading);
+        setPosts(data);
+      } catch (error) {}
+    };
+    loadPosts();
+  }, []);
+
   return (
     <View>
-      <Text>Home Screen</Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#007bff" />
+      ) : (
+        <FlatList
+          data={posts}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.title}</Text>
+              <Text>{item.body}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
-export default HomeScene;
+export default HomeScreen;
