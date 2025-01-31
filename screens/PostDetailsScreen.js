5980a25d3bceaee342c62cc813a1bf0532204162
Ankur Halder <ankur.halder12345@gmail.com>
2025-01-31 16:09:52 +0530

    feat: enhance PostDetailsScreen to display additional user information including age, gender, and occupation

--- screens/PostDetailsScreen.js
+++ screens/PostDetailsScreen.js
@@ -1,80 +1,54 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 
 const ProfileDetailScreen = ({ route }) => {
   const { userData } = route.params || {};
 
   if (!userData) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.errorText}>No User Data Available</Text>
+      <View>
+        <Text>No User Data Available</Text>
       </View>
     );
   }
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Image
-        source={{
-          uri: userData.image || "https://www.ankurhalder.in/apple-icon.png",
-        }}
-        style={styles.profileImage}
-      />
-      <Text style={styles.name}>{`${userData.firstName} ${
-        userData.maidenName ? userData.maidenName + " " : ""
-      }${userData.lastName}`}</Text>
-      <Text style={styles.username}>@{userData.username}</Text>
-      <Text style={styles.email}>Email: {userData.email}</Text>
-      <Text style={styles.phone}>Phone: {userData.phone}</Text>
-      <Text style={styles.address}>
-        Address: {userData.address?.address}, {userData.address?.city},
-        {userData.address?.state} , {userData.address?.country}
-      </Text>
+    <ScrollView contentContainer>
+      <View>
+        <Image
+          source={{
+            uri: userData.image || "https://www.ankurhalder.in/apple-icon.png",
+          }}
+        />
+      </View>
+      <View>
+        <Text>{`${userData.firstName} ${
+          userData.maidenName ? userData.maidenName + " " : ""
+        }${userData.lastName}`}</Text>
+        <Text>@{userData.username}</Text>
+      </View>
+      <View>
+        <Text>Email: {userData.email}</Text>
+        <Text>Phone: {userData.phone}</Text>
+        <Text>Age : {userData.age}</Text>
+        <Text>Gender : {userData.gender}</Text>
+      </View>
+      <View>
+        <Text>
+          Occupation: {userData.company?.department} at {userData.company?.name}{" "}
+          as
+          {userData.company?.title}
+        </Text>
+      </View>
+      <View>
+        <Text>
+          Address: {userData.address?.address}, {userData.address?.city},
+          {userData.address?.state} , {userData.address?.country}
+        </Text>
+      </View>
       {console.log("Full User Data:", userData)}
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    padding: 20,
-  },
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 20,
-  },
-  name: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  username: {
-    fontSize: 18,
-    color: "gray",
-    marginBottom: 10,
-  },
-  email: {
-    fontSize: 16,
-  },
-  phone: {
-    fontSize: 16,
-    color: "gray",
-  },
-  address: {
-    fontSize: 14,
-    color: "gray",
-    textAlign: "center",
-    marginTop: 5,
-  },
-  errorText: {
-    fontSize: 18,
-    color: "red",
-  },
-});
 
 export default ProfileDetailScreen;
