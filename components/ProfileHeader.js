a23237cb8c16db1566ba5bb14a2cb7fbc2ab7132
Ankur Halder <ankur.halder12345@gmail.com>
2025-02-03 14:54:00 +0530

    feat: change ProfileHeader layout to use column direction for profile image container

--- components/ProfileHeader.js
+++ components/ProfileHeader.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
   },
   coverImage: { width: "100%", height: 100, marginBottom: -50 },
   profileImageContainer: {
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
   },
   profileImage: {
